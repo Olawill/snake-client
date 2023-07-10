c85577ad15a6182cefbec5d8773c91bb7d2c7f41
Olawale <henry.williams658@gmail.com>
2023-07-10 22:58:01 +0000

    Separated the connet function into its own file for modularity of code, also exported the function and imported it in the plays.js file

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // handle incoming data
+  conn.on('data', (chunk) => console.log("you ded cus you idled"));
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+
+};
+
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,21 +1,21 @@
-const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
+// // establishes a connection with the game server
+// const connect = function() {
+//   const conn = net.createConnection({
+//     host: 'localhost',
+//     port: 50541
+//   });
 
-  // handle incoming data
-  conn.on('data', (chunk) => console.log("you ded cus you idled"));
+//   // handle incoming data
+//   conn.on('data', (chunk) => console.log("you ded cus you idled"));
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
+//   return conn;
 
-};
+// };
 
 console.log("Connecting ...");
 connect();
