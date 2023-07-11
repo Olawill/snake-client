ebfa08b9f5b72cf2e4ba4f7e3816140e598da1de
Olawale <henry.williams658@gmail.com>
2023-07-11 23:53:24 +0000

    Defined a function registered as a data callback for standard input, defined the Ctrl+c input case

--- play.js
+++ play.js
@@ -1,6 +1,27 @@
 // inport the connect function for the client file
 const { connect } = require("./client");
 
+// Setting up for user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // register event listener for user input
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+// data callback handler
+const handleUserInput = function (key) {
+  // check for ctrl + c and terminate the game
+  if (key === '\u0003') {
+    process.exit(0);
+  }
+};
 
 console.log("Connecting ...");
 connect();
+setupInput();
