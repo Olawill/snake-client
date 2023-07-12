7d366a4d99372085e41fac797c34f3289831f7c9
Olawale <henry.williams658@gmail.com>
2023-07-12 01:06:19 +0000

    Refactored the play.js file, created the input.js file to handle user inputs and exported the functions

---
+++ input.js
@@ -0,0 +1,22 @@
+// Setting up for user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // register event listener for user input
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+// data callback handler
+const handleUserInput = function (key) {
+  // check for ctrl + c and terminate the game
+  if (key === '\u0003') {
+    process.exit(0);
+  }
+};
+
+module.exports = { setupInput };
