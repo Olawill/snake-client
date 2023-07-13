250b58ea1d00d10a23629ffb76259ee23eaf4c5c
Olawale <henry.williams658@gmail.com>
2023-07-13 03:39:28 +0000

    Edited the play.js file to use the connnection object, also added movements to the inputs.js file

--- input.js
+++ input.js
@@ -1,5 +1,11 @@
+// Stores the active TCP connection object.
+let connection;
+
 // Setting up for user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -16,6 +22,26 @@ const handleUserInput = function (key) {
   // check for ctrl + c and terminate the game
   if (key === '\u0003') {
     process.exit(0);
+  }
+
+  if (key === 'w') {
+    // Moving up
+    connection.write('Move: up');
+  }
+
+  if (key === 'a') {
+    // Moving Left
+    connection.write('Move: left');
+  }
+
+  if (key === 's') {
+    // Moving Down
+    connection.write('Move: down');
+  }
+
+  if (key === 'd') {
+    // Moving Right
+    connection.write('Move: right');
   }
 };
 
